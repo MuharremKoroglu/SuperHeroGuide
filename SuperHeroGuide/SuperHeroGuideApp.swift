2e7ef06bd63cf9a5c6f001c0335e051e841fb655
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2023-04-02 09:12:23 +0300

    Initial Commit

---
+++ SuperHeroGuide/SuperHeroGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroGuideApp.swift
+//  SuperHeroGuide
+//
+//  Created by Muharrem Köroğlu on 2.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
